a78dccf09f6201e0b3d759a81e6b70b6c7a903d3
AndreyAleksandrov48 <121509917+AndreyAleksandrov48@users.noreply.github.com>
2023-08-03 10:39:40 +0300

    Initial Commit

---
+++ RandomCameraPlayer/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  RandomCameraPlayer
+//
+//  Created by Anonymous on 31/07/2023.
+//
+
+import SwiftUI
+import AVKit
+
+
+struct ContentView: View{
+    var body: some View{
+            ZStack{
+                Text("My App")
+            
+        }
+    }
+}
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{
+        ContentView()
+    }
+    }
---
+++ RandomCameraPlayer/RandomCameraPlayerApp.swift
@@ -0,0 +1,18 @@
+//
+//  RandomCameraPlayerApp.swift
+//  RandomCameraPlayer
+//
+//  Created by Anonymous on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomCameraPlayerApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
